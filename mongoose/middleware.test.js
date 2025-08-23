66c4f09c4e5262262dce3932e32f899bd21c2bce
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2025-08-23 15:28:33 +0530

    Mongoose Middleware

---
+++ mongoose/middleware.test.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+//! Step 1: to connect to the mongodb server
+try {
+  await mongoose.connect("mongodb://127.0.0.1/mongoose_middleware");
+  mongoose.set("debug", true);
+} catch (error) {
+  console.error(error);
+  process.exit();
+}
+
+//! Step 2: Create Schema
+const userSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  age: { type: Number, required: true, min: 5 },
+  createdAt: { type: Date, default: Date.now() },
+  updatedAt: { type: Date, default: Date.now() },
+});
+
+//! We will use middleware here
+userSchema.pre(
+  ["updateOne", "updateMany", "findOneAndUpdate"],
+  function (next) {
+    this.set({ updatedAt: Date.now() });
+    next();
+  }
+);
+
+//! Create a model
+const Users = mongoose.model("user", userSchema);
+
+// await Users.create({
+//   name: "Dharmesh Yadav",
+//   age: 21,
+//   email: "yadavdharmesh@gmail.com",
+// });
+
+//! To Update the data:
+await Users.updateOne(
+  { email: "yadavdharmesh@gmail.com" },
+  { $set: { age: 20 } }
+);
+
+await mongoose.connection.close();
